42e5a98dc7a11a8e544778bef1e5408bee860efd
Gustavo Rocha <gustavo@onserp.com.br>
2020-07-11 02:23:53 -0300

    teste netlify.toml

--- src/main.js
+++ src/main.js
@@ -13,9 +13,4 @@ export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
   head.htmlAttrs = { lang: 'pt-BR' }
-  head.link.push({
-    rel: 'stylesheet',
-    href:
-      'https://fonts.googleapis.com/css2?family=Merriweather&family=Open+Sans:wght@600&family=Raleway:wght@400;700&display=swap'
-  })
 }
